d86afe453f0eb6b0b948966e9d36ad18472b74ec
RichardLimaDxD <richardpires663802@gmail.com>
2025-07-14 18:51:52 -0300

    feat: validate check-in use case - tests

--- src/__tests__/useCases/check-in/validateCheck-inUseCase.spec.ts
+++ src/__tests__/useCases/check-in/validateCheck-inUseCase.spec.ts
@@ -1,7 +1,7 @@
 import InMemoryCheckInRepository from "@/__tests__/in-memory/check-in/check-in.in-memory.repository";
 import { ValidateCheckInUseCase } from "@/application/useCases/check-in/validateCheck-inUseCase";
 import { AppError } from "@/error";
-import { beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 let checkInsRepository: InMemoryCheckInRepository;
 let validateCheckInUseCase: ValidateCheckInUseCase;
@@ -10,6 +10,11 @@ describe("Validate Check-in Use Case", () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInRepository();
     validateCheckInUseCase = new ValidateCheckInUseCase(checkInsRepository);
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it("should be able to validate the check-in", async () => {
@@ -32,5 +37,24 @@ describe("Validate Check-in Use Case", () => {
         checkInId: "inexistent-check-in-id",
       }),
     ).rejects.toThrowError(new AppError("Check-in not found", 404));
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesInMs: number = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      validateCheckInUseCase.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toThrowError(new AppError("Check-in expired", 400));
   });
 });
--- src/application/useCases/check-in/validateCheck-inUseCase.ts
+++ src/application/useCases/check-in/validateCheck-inUseCase.ts
@@ -4,6 +4,7 @@ import {
   ValidateCheckInUseCaseResponse,
 } from "@/interfaces/check-in.interface";
 import CheckInRepository from "@/repositories/check-in.repository";
+import dayjs from "dayjs";
 
 export class ValidateCheckInUseCase {
   constructor(private checkInRepository: CheckInRepository) {}
@@ -14,6 +15,14 @@ export class ValidateCheckInUseCase {
     const checkIn = await this.checkInRepository.findById(checkInId);
 
     if (!checkIn) throw new AppError("Check-in not found", 404);
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      "minutes",
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20)
+      throw new AppError("Check-in expired", 400);
 
     checkIn.validated_at = new Date();
 
